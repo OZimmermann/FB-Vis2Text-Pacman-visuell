cd84a7132f1585a5571b35aaea97d116fe52e867
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 18:47:36 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,37 @@
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
-        while (!(input.buttonIsPressed(Button.B))) {
-            basic.showLeds(`
-                # . # # .
-                . # . # #
-                . . # # #
-                . # # # #
-                # . # # .
-                `)
-            basic.pause(100)
-            basic.showLeds(`
-                . . # # .
-                # # . # #
-                # # # # #
-                # # # # #
-                . . # # .
-                `)
-            basic.pause(100)
-        }
+        basic.showLeds(`
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            `)
+        basic.pause(100)
     } else if (input.buttonIsPressed(Button.B)) {
-        while (!(input.buttonIsPressed(Button.A))) {
-            basic.showLeds(`
-                . # # . #
-                # # . # .
-                # # # . .
-                # # # # .
-                . # # . #
-                `)
-            basic.pause(100)
-            basic.showLeds(`
-                . # # . .
-                # # . # #
-                # # # # #
-                # # # # #
-                . # # . .
-                `)
-            basic.pause(100)
-        }
+        basic.showLeds(`
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            `)
+        basic.pause(100)
     }
 }
