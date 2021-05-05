e7b1273a17ae02d57d2f6a1fc23db20ee145a731
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 18:33:12 +0200

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 while True:
     if input.button_is_pressed(Button.A):
-        basic.show_leds("""
-            # . # # .
-            . # . # #
-            . . # # #
-            . # # # #
-            # . # # .
-            """)
-        basic.pause(100)
-        basic.show_leds("""
-            . . # # .
-            # # . # #
-            # # # # #
-            # # # # #
-            . . # # .
-            """)
-        basic.pause(100)
+        while not (input.button_is_pressed(Button.B)) and not (input.is_gesture(Gesture.SHAKE)):
+            basic.show_leds("""
+                # . # # .
+                . # . # #
+                . . # # #
+                . # # # #
+                # . # # .
+                """)
+            basic.pause(100)
+            basic.show_leds("""
+                . . # # .
+                # # . # #
+                # # # # #
+                # # # # #
+                . . # # .
+                """)
+            basic.pause(100)
     elif input.button_is_pressed(Button.B):
-        basic.show_leds("""
-            . # # . #
-            # # . # .
-            # # # . .
-            # # # # .
-            . # # . #
-            """)
-        basic.pause(100)
-        basic.show_leds("""
-            . # # . .
-            # # . # #
-            # # # # #
-            # # # # #
-            . # # . .
-            """)
-        basic.pause(100)
-    else:
+        while not (input.button_is_pressed(Button.A)) and not (input.is_gesture(Gesture.SHAKE)):
+            basic.show_leds("""
+                . # # . #
+                # # . # .
+                # # # . .
+                # # # # .
+                . # # . #
+                """)
+            basic.pause(100)
+            basic.show_leds("""
+                . # # . .
+                # # . # #
+                # # # # #
+                # # # # #
+                . # # . .
+                """)
+            basic.pause(100)
+    elif input.is_gesture(Gesture.SHAKE):
         basic.show_icon(IconNames.HAPPY)
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
-        while (!(input.buttonIsPressed(Button.B))) {
+        while (!(input.buttonIsPressed(Button.B)) && !(input.isGesture(Gesture.Shake))) {
             basic.showLeds(`
                 # . # # .
                 . # . # #
@@ -18,5 +18,26 @@ while (true) {
                 `)
             basic.pause(100)
         }
+    } else if (input.buttonIsPressed(Button.B)) {
+        while (!(input.buttonIsPressed(Button.A)) && !(input.isGesture(Gesture.Shake))) {
+            basic.showLeds(`
+                . # # . #
+                # # . # .
+                # # # . .
+                # # # # .
+                . # # . #
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . # # . .
+                # # . # #
+                # # # # #
+                # # # # #
+                . # # . .
+                `)
+            basic.pause(100)
+        }
+    } else if (input.isGesture(Gesture.Shake)) {
+        basic.showIcon(IconNames.Happy)
     }
 }
