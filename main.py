5fe6752a17b5510d6ea8616ecf3cdbb566329ed1
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 18:12:58 +0200

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-while not (input.button_is_pressed(Button.AB)):
+while True:
     if input.button_is_pressed(Button.A):
-        basic.show_leds("""
-            . # # . #
-            # # . # .
-            # # # . .
-            # # # # .
-            . # # . #
-            """)
-        basic.pause(100)
-        basic.show_leds("""
-            . # # . .
-            # # . # #
-            # # # # #
-            # # # # #
-            . # # . .
-            """)
-        basic.pause(100)
-    elif input.button_is_pressed(Button.B):
         basic.show_leds("""
             # . # # .
             . # . # #
@@ -33,10 +16,22 @@ while not (input.button_is_pressed(Button.AB)):
             . . # # .
             """)
         basic.pause(100)
+    elif input.button_is_pressed(Button.B):
+        basic.show_leds("""
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            """)
+        basic.pause(100)
+        basic.show_leds("""
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            """)
+        basic.pause(100)
     else:
         basic.show_icon(IconNames.HAPPY)
-for index in range(4):
-    basic.show_icon(IconNames.HEART)
-    basic.pause(100)
-    basic.show_icon(IconNames.SMALL_HEART)
-    basic.pause(100)
--- main.ts
+++ main.ts
@@ -1,54 +1,20 @@
-function herzschlag (num: number) {
-    for (let index = 0; index < num; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(100)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.pause(100)
-    }
-}
-function pacmanlinks () {
-    basic.showLeds(`
-        # . # # .
-        . # . # #
-        . . # # #
-        . # # # #
-        # . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # # .
-        # # . # #
-        # # # # #
-        # # # # #
-        . . # # .
-        `)
-    basic.pause(100)
-}
-function pacmanrechts () {
-    basic.showLeds(`
-        . # # . #
-        # # . # .
-        # # # . .
-        # # # # .
-        . # # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . .
-        # # . # #
-        # # # # #
-        # # # # #
-        . # # . .
-        `)
-    basic.pause(100)
-}
-while (!(input.buttonIsPressed(Button.AB))) {
+while (true) {
     if (input.buttonIsPressed(Button.A)) {
-        pacmanrechts()
-    } else if (input.buttonIsPressed(Button.B)) {
-        pacmanlinks()
-    } else {
-        basic.showIcon(IconNames.Happy)
+        basic.showLeds(`
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            `)
+        basic.pause(100)
     }
 }
-herzschlag(4)
