f024359f44cbb557202895a74784586ed6e5765e
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 18:43:33 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,43 @@
 while (true) {
-    basic.showLeds(`
-        # . # # .
-        . # . # #
-        . . # # #
-        . # # # #
-        # . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # # .
-        # # . # #
-        # # # # #
-        # # # # #
-        . . # # .
-        `)
-    basic.pause(100)
+    if (input.buttonIsPressed(Button.A)) {
+        while (!(input.buttonIsPressed(Button.B)) && !(input.isGesture(Gesture.Shake))) {
+            basic.showLeds(`
+                # . # # .
+                . # . # #
+                . . # # #
+                . # # # #
+                # . # # .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . # # .
+                # # . # #
+                # # # # #
+                # # # # #
+                . . # # .
+                `)
+            basic.pause(100)
+        }
+    } else if (input.buttonIsPressed(Button.B)) {
+        while (!(input.buttonIsPressed(Button.A)) && !(input.isGesture(Gesture.Shake))) {
+            basic.showLeds(`
+                . # # . #
+                # # . # .
+                # # # . .
+                # # # # .
+                . # # . #
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . # # . .
+                # # . # #
+                # # # # #
+                # # # # #
+                . # # . .
+                `)
+            basic.pause(100)
+        }
+    } else if (input.isGesture(Gesture.Shake)) {
+        basic.showIcon(IconNames.Happy)
+    }
 }
-basic.forever(function () {
-    basic.showLeds(`
-        # . # # .
-        . # . # #
-        . . # # #
-        . # # # #
-        # . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # # .
-        # # . # #
-        # # # # #
-        # # # # #
-        . . # # .
-        `)
-    basic.pause(100)
-})
