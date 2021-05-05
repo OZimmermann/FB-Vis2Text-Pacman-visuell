62a049444bed0004eadda12400e6b2b77a14fbe9
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 08:21:22 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,45 @@
-function herzschlag (num: number) {
-    for (let index = 0; index < num; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(100)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.pause(100)
-    }
-}
-function pacmanlinks () {
-    basic.showLeds(`
-        # . # # .
-        . # . # #
-        . . # # #
-        . # # # #
-        # . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # # .
-        # # . # #
-        # # # # #
-        # # # # #
-        . . # # .
-        `)
-    basic.pause(100)
-}
-function pacmanrechts () {
-    basic.showLeds(`
-        . # # . #
-        # # . # .
-        # # # . .
-        # # # # .
-        . # # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . .
-        # # . # #
-        # # # # #
-        # # # # #
-        . # # . .
-        `)
-    basic.pause(100)
-}
 while (!(input.buttonIsPressed(Button.AB))) {
     if (input.buttonIsPressed(Button.A)) {
-        pacmanrechts()
+        basic.showLeds(`
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            `)
+        basic.pause(100)
     } else if (input.buttonIsPressed(Button.B)) {
-        pacmanlinks()
+        basic.showLeds(`
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            `)
+        basic.pause(100)
     } else {
         basic.showIcon(IconNames.Happy)
     }
 }
-herzschlag(4)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
