2d768fa4ef6af32ad35bc3f4db959485f996bba7
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-05 18:46:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
-        while (true) {
-            basic.showLeds(`
-                # . # # .
-                . # . # #
-                . . # # #
-                . # # # #
-                # . # # .
-                `)
-            basic.pause(100)
-            basic.showLeds(`
-                . . # # .
-                # # . # #
-                # # # # #
-                # # # # #
-                . . # # .
-                `)
-            basic.pause(100)
-        }
+        basic.showLeds(`
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            `)
+        basic.pause(100)
     }
 }
