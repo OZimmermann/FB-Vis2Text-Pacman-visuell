2ba159def8ff501386bcce00ccdbf3c1f2fe8637
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-10 00:09:06 +0200

    Aktualisiere main.py

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 while True:
     if input.button_is_pressed(Button.A):
-        while not (input.button_is_pressed(Button.B)) and not (input.is_gesture(Gesture.SHAKE)):
-            basic.show_leds("""
-                # . # # .
-                . # . # #
-                . . # # #
-                . # # # #
-                # . # # .
-                """)
-            basic.pause(100)
-            basic.show_leds("""
-                . . # # .
-                # # . # #
-                # # # # #
-                # # # # #
-                . . # # .
-                """)
-            basic.pause(100)
+        basic.show_leds("""
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
+            """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            """)
+        basic.pause(100)
     elif input.button_is_pressed(Button.B):
-        while not (input.button_is_pressed(Button.A)) and not (input.is_gesture(Gesture.SHAKE)):
-            basic.show_leds("""
-                . # # . #
-                # # . # .
-                # # # . .
-                # # # # .
-                . # # . #
-                """)
-            basic.pause(100)
-            basic.show_leds("""
-                . # # . .
-                # # . # #
-                # # # # #
-                # # # # #
-                . # # . .
-                """)
-            basic.pause(100)
-    elif input.is_gesture(Gesture.SHAKE):
-        basic.show_icon(IconNames.HAPPY)
+        basic.show_leds("""
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            """)
+        basic.pause(100)
+        basic.show_leds("""
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            """)
+        basic.pause(100)
