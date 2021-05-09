1cf2684d6d61fc505dcd73b43bb159d57353f59b
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-10 00:08:07 +0200

    Aktualisiere main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-while not (input.button_is_pressed(Button.AB)):
-    if input.button_is_pressed(Button.A):
-        basic.show_leds("""
-            . # # . #
-            # # . # .
-            # # # . .
-            # # # # .
-            . # # . #
-            """)
+def herzschlag(num: number):
+    for index in range(num):
+        basic.show_icon(IconNames.HEART)
         basic.pause(100)
-        basic.show_leds("""
-            . # # . .
-            # # . # #
-            # # # # #
-            # # # # #
-            . # # . .
-            """)
+        basic.show_icon(IconNames.SMALL_HEART)
         basic.pause(100)
-    elif input.button_is_pressed(Button.B):
+def pacmanlinks():
+    while not (input.button_is_pressed(Button.B)):
         basic.show_leds("""
             # . # # .
             . # . # #
@@ -33,10 +22,28 @@ while not (input.button_is_pressed(Button.AB)):
             . . # # .
             """)
         basic.pause(100)
-    else:
-        basic.show_icon(IconNames.HAPPY)
-for index in range(4):
-    basic.show_icon(IconNames.HEART)
-    basic.pause(100)
-    basic.show_icon(IconNames.SMALL_HEART)
-    basic.pause(100)
+def pacmanrechts():
+    while not (input.button_is_pressed(Button.A)):
+        basic.show_leds("""
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            """)
+        basic.pause(100)
+        basic.show_leds("""
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            """)
+        basic.pause(100)
+while True:
+    while not (input.button_is_pressed(Button.AB)):
+        if input.button_is_pressed(Button.A):
+            pacmanlinks()
+        elif input.button_is_pressed(Button.B):
+            pacmanrechts()
+    herzschlag(4)
