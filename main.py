b3825a2bfcfd751b08afa49d8045a04241f25fba
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-16 21:39:59 +0200

    Aktualisiere main.py

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
+def herzschlag(num: number):
+    for index in range(num):
+        basic.show_icon(IconNames.HEART)
+        basic.pause(100)
+        basic.show_icon(IconNames.SMALL_HEART)
+        basic.pause(100)
+def pacmanlinks():
+    basic.show_leds("""
+        # . # # .
+        . # . # #
+        . . # # #
+        . # # # #
+        # . # # .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # # .
+        # # . # #
+        # # # # #
+        # # # # #
+        . . # # .
+        """)
+    basic.pause(100)
+def pacmanrechts():
+    basic.show_leds("""
+        . # # . #
+        # # . # .
+        # # # . .
+        # # # # .
+        . # # . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . # # . .
+        # # . # #
+        # # # # #
+        # # # # #
+        . # # . .
+        """)
+    basic.pause(100)
 while not (input.button_is_pressed(Button.AB)):
     if input.button_is_pressed(Button.A):
-        basic.show_leds("""
-            . # # . #
-            # # . # .
-            # # # . .
-            # # # # .
-            . # # . #
-            """)
-        basic.pause(100)
-        basic.show_leds("""
-            . # # . .
-            # # . # #
-            # # # # #
-            # # # # #
-            . # # . .
-            """)
-        basic.pause(100)
+        pacmanrechts()
     elif input.button_is_pressed(Button.B):
-        basic.show_leds("""
-            # . # # .
-            . # . # #
-            . . # # #
-            . # # # #
-            # . # # .
-            """)
-        basic.pause(100)
-        basic.show_leds("""
-            . . # # .
-            # # . # #
-            # # # # #
-            # # # # #
-            . . # # .
-            """)
-        basic.pause(100)
+        pacmanlinks()
     else:
         basic.show_icon(IconNames.HAPPY)
-for index in range(4):
-    basic.show_icon(IconNames.HEART)
-    basic.pause(100)
-    basic.show_icon(IconNames.SMALL_HEART)
-    basic.pause(100)
+herzschlag(4)
