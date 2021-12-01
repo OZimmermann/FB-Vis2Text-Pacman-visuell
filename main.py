11e2cb52bae4ce78f879b613cf49c1ac30d6179a
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-12-01 13:17:48 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
-def herzschlag(num: number):
-    for index in range(num):
+def on_forever():
+    while not (input.button_is_pressed(Button.AB)):
+        if input.button_is_pressed(Button.A):
+            while not (input.button_is_pressed(Button.B)):
+                basic.show_leds("""
+                    # . # # .
+                    . # . # #
+                    . . # # #
+                    . # # # #
+                    # . # # .
+                    """)
+                basic.pause(100)
+                basic.show_leds("""
+                    . . # # .
+                    # # . # #
+                    # # # # #
+                    # # # # #
+                    . . # # .
+                    """)
+                basic.pause(100)
+        if input.button_is_pressed(Button.B):
+            while not (input.button_is_pressed(Button.A)):
+                basic.show_leds("""
+                    . # # . #
+                    # # . # .
+                    # # # . .
+                    # # # # .
+                    . # # . #
+                    """)
+                basic.pause(100)
+                basic.show_leds("""
+                    . # # . .
+                    # # . # #
+                    # # # # #
+                    # # # # #
+                    . # # . .
+                    """)
+                basic.pause(100)
+    for index in range(4):
         basic.show_icon(IconNames.HEART)
         basic.pause(100)
         basic.show_icon(IconNames.SMALL_HEART)
         basic.pause(100)
-def pacmanlinks():
-    basic.show_leds("""
-        # . # # .
-        . # . # #
-        . . # # #
-        . # # # #
-        # . # # .
-        """)
-    basic.pause(100)
-    basic.show_leds("""
-        . . # # .
-        # # . # #
-        # # # # #
-        # # # # #
-        . . # # .
-        """)
-    basic.pause(100)
-def pacmanrechts():
-    basic.show_leds("""
-        . # # . #
-        # # . # .
-        # # # . .
-        # # # # .
-        . # # . #
-        """)
-    basic.pause(100)
-    basic.show_leds("""
-        . # # . .
-        # # . # #
-        # # # # #
-        # # # # #
-        . # # . .
-        """)
-    basic.pause(100)
-while not (input.button_is_pressed(Button.AB)):
-    if input.button_is_pressed(Button.A):
-        pacmanrechts()
-    elif input.button_is_pressed(Button.B):
-        pacmanlinks()
-    else:
-        basic.show_icon(IconNames.HAPPY)
-herzschlag(4)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,58 +1,92 @@
-function herzschlag (num: number) {
-    for (let index = 0; index < num; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(100)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.pause(100)
-    }
-}
-function pacmanlinks () {
-    while (!(input.buttonIsPressed(Button.B))) {
-        basic.showLeds(`
-            # . # # .
-            . # . # #
-            . . # # #
-            . # # # #
-            # . # # .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . # # .
-            # # . # #
-            # # # # #
-            # # # # #
-            . . # # .
-            `)
-        basic.pause(100)
-    }
-}
-function pacmanrechts () {
-    while (!(input.buttonIsPressed(Button.A))) {
-        basic.showLeds(`
-            . # # . #
-            # # . # .
-            # # # . .
-            # # # # .
-            . # # . #
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . # # . .
-            # # . # #
-            # # # # #
-            # # # # #
-            . # # . .
-            `)
-        basic.pause(100)
-    }
-}
-while (true) {
-    while (!(input.buttonIsPressed(Button.AB))) {
-        if (input.buttonIsPressed(Button.A)) {
-            pacmanlinks()
-        } else if (input.buttonIsPressed(Button.B)) {
-            pacmanrechts()
+while (!(input.buttonIsPressed(Button.AB))) {
+    if (input.buttonIsPressed(Button.A)) {
+        while (!(input.buttonIsPressed(Button.B))) {
+            basic.showLeds(`
+                # . # # .
+                . # . # #
+                . . # # #
+                . # # # #
+                # . # # .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . # # .
+                # # . # #
+                # # # # #
+                # # # # #
+                . . # # .
+                `)
+            basic.pause(100)
         }
     }
-    herzschlag(4)
+    if (input.buttonIsPressed(Button.B)) {
+        while (!(input.buttonIsPressed(Button.A))) {
+            basic.showLeds(`
+                . # # . #
+                # # . # .
+                # # # . .
+                # # # # .
+                . # # . #
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . # # . .
+                # # . # #
+                # # # # #
+                # # # # #
+                . # # . .
+                `)
+            basic.pause(100)
+        }
+    }
 }
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
+basic.forever(function () {
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            while (!(input.buttonIsPressed(Button.B))) {
+                basic.showLeds(`
+                    # . # # .
+                    . # . # #
+                    . . # # #
+                    . # # # #
+                    # . # # .
+                    `)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . # # .
+                    # # . # #
+                    # # # # #
+                    # # # # #
+                    . . # # .
+                    `)
+                basic.pause(100)
+            }
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            while (!(input.buttonIsPressed(Button.A))) {
+                basic.showLeds(`
+                    . # # . #
+                    # # . # .
+                    # # # . .
+                    # # # # .
+                    . # # . #
+                    `)
+                basic.pause(100)
+                basic.showLeds(`
+                    . # # . .
+                    # # . # #
+                    # # # # #
+                    # # # # #
+                    . # # . .
+                    `)
+                basic.pause(100)
+            }
+        }
+    }
+})
